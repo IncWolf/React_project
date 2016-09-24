3d6dda825be7de53aceab2cfa4862e8c8ccc4c12
IncWolf <incwolf@gmail.com>
2016-09-24 18:41:38 +0300

    functionality of the Company block

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -2,55 +2,66 @@
  * Created by galushkasergiy on 24.09.16.
  */
 var React = require('react');
+var ReactCSSTransitionGroup = require('react-addons-css-transition-group');
 
 class Skills extends React.Component {
-    constructor(props) {
-        super(props);
-        this.step = this.step.bind(this);
-        this.stop = this.stop.bind(this);
-    }
-    step() {
-        var elements = document.querySelectorAll(".progress-bar");
-        for (var i=0; i<elements.length;i++) {
-            elements[i].style.width = parseInt(elements[i].style.width) + Math.floor(elements[i].dataset.max/30) + '%';
-        }
-    }
-    stop() {
-        clearInterval(this.interval);
-    }
-    componentDidMount() {
-        this.interval = setInterval(this.step, 100);
-        this.timeout = setTimeout(this.stop, 3000);
-    }
-    componentWillUnmount() {
-        this.stop();
-    }
+    //constructor(props) {
+    //    super(props);
+    //    this.step = this.step.bind(this);
+    //    this.stop = this.stop.bind(this);
+    //}
+    //step() {
+    //    var elements = document.querySelectorAll(".progress-bar");
+    //    for (var i=0; i<elements.length;i++) {
+    //        elements[i].style.width = parseInt(elements[i].style.width) + Math.floor(elements[i].dataset.max/30) + '%';
+    //    }
+    //}
+    //stop() {
+    //    clearInterval(this.interval);
+    //}
+    //componentDidMount() {
+    //    this.interval = setInterval(this.step, 100);
+    //    this.timeout = setTimeout(this.stop, 3000);
+    //}
+    //componentWillUnmount() {
+    //    this.stop();
+    //}
     render() {
         return (<div>
             <div className="progress">
-                <div className="progress-bar" role="progressbar" data-max="80" style={{width:1+'%'}}>
-                    USER INTERFACE
-                </div>
+                <ReactCSSTransitionGroup transitionName="max80" transitionAppear={true} transitionAppearTimeout={3000} transitionEnterTimeout={3000} >
+                    <div className="progress-bar max80" role="progressbar">
+                        USER INTERFACE
+                    </div>
+                </ReactCSSTransitionGroup>
             </div>
             <div className="progress">
-                <div className="progress-bar" role="progressbar" data-max="90" style={{width:1+'%'}}>
-                    WEB DESIGN
-                </div>
+                <ReactCSSTransitionGroup transitionName="max90" transitionAppear={true} transitionAppearTimeout={3000} transitionEnterTimeout={3000}>
+                    <div className="progress-bar max90" role="progressbar">
+                        WEB DESIGN
+                    </div>
+                </ReactCSSTransitionGroup>
             </div>
             <div className="progress">
-                <div className="progress-bar" role="progressbar" data-max="75" style={{width:1+'%'}}>
-                    WORDPRESS
-                </div>
+                <ReactCSSTransitionGroup transitionName="max75" transitionAppear={true} transitionAppearTimeout={3000} transitionEnterTimeout={3000}>
+                    <div className="progress-bar max75" role="progressbar">
+                        WORDPRESS
+                    </div>
+                </ReactCSSTransitionGroup>
             </div>
             <div className="progress">
-                <div className="progress-bar" role="progressbar" data-max="94" style={{width:1+'%'}}>
-                    HTML & CSS
-                </div>
+                <ReactCSSTransitionGroup transitionName="max94" transitionAppear={true} transitionAppearTimeout={3000} transitionEnterTimeout={3000}>
+                    <div className="progress-bar max94" role="progressbar">
+                        HTML & CSS
+                    </div>
+                </ReactCSSTransitionGroup>
             </div>
             <div className="progress">
-                <div className="progress-bar" role="progressbar"data-max="87" style={{width:1+'%'}}>
-                    APP DESIGN
-                </div>
+                <ReactCSSTransitionGroup transitionName="max87" transitionAppear={true} transitionAppearTimeout={3000} transitionEnterTimeout={3000}>
+                    <div className="progress-bar max87" role="progressbar">
+                        APP DESIGN
+                    </div>
+                </ReactCSSTransitionGroup>
             </div>
         </div>)
     }
